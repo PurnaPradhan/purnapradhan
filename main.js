a7921cf0674bba0c71bb5c3b99e79617a06ce0dc
PurnaPradhan <purna.virtual@gmail.com>
2024-07-10 14:35:31 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Fetch and display the visit count
+    fetch('counter.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('visit-count').innerText = data.trim();
+        })
+        .catch(error => {
+            console.error('Error fetching visit count:', error);
+        });
+
+
 document.querySelectorAll('.menu a').forEach(item => {
     item.addEventListener('click', () => {
         document.getElementById('menu-toggle').checked = false;
