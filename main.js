dd368e3a8833cf35d772e181e7debed94c5554fd
PurnaPradhan <purna.virtual@gmail.com>
2024-07-25 19:56:24 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('counter.txt')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('visit-count').innerText = data.trim();
-        })
-        .catch(error => {
-            console.error('Error fetching visit count:', error);
-        });
-});
-
 document.querySelectorAll('.menu a').forEach(item => {
     item.addEventListener('click', () => {
         document.getElementById('menu-toggle').checked = false;
