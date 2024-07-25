1e830f3717952fd74d1a263959f20f8fd7ba097f
PurnaPradhan <purna.virtual@gmail.com>
2024-07-25 20:57:11 +0545

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ document.querySelectorAll('.menu a').forEach(item => {
         document.getElementById('menu-toggle').checked = false;
     });
 });
+
+function showContent(id) {
+    // Hide all content boxes
+    document.querySelectorAll('.content .box').forEach(box => {
+        box.style.display = 'none';
+    });
+    
+    // Show the selected content box
+    const contentBox = document.getElementById(id);
+    if (contentBox) {
+        contentBox.style.display = 'block';
+}
+}
+
